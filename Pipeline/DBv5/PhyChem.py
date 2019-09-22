a1f47cbb362b00b99154f4793ecbf100db745872
ame <zhxmain@foxmail.com>
2019-09-22 19:27:44 +0800

    update

--- Pipeline/DBv5/PhyChem.py
+++ Pipeline/DBv5/PhyChem.py
@@ -1,27 +1,18 @@
 import os
-#os.chdir('root directory')
-from sklearn.model_selection import StratifiedKFold, train_test_split
-
-from Datas.Datasets import Datasets
+#os.chdir('your root directory')
+import pickle
+from Pipeline import tools
 from Model.CNN.CnnModel import CNNModel
-from Result.Train.EvalModel import EvalModel
-
+WINSIZE = 8
 def trainPhychem():
-    #winsizelist=[5,6,7,8,9,10]
-    winsizelist = [8]  # the window size is 8*2+1=17
-    for size in winsizelist:
-        data, label = Datasets.getPhyChem('DBv5', size)
-        skf=StratifiedKFold(n_splits=10,random_state=0,shuffle=True)
-        eval=EvalModel()
-        modelIdx=0
-        for train,test in skf.split(data,label):
-            x_train=data[train];x_test=data[test]
-            y_train=label[train];y_test=label[test]
-            model=CNNModel.CNN1D(shape=(size*2+1,10))
-            modelIdx+=1
-            model.fit(x_train,y_train,batch_size=100,epochs=100,
+    for i in range(10):
+        phychem_train_data, phychem_train_label = pickle.load("/Dataset/DBv5/phychem/phychem-train-" + str(i + 1) + ".pkl")
+        phychem_test_data, phychem_test_label = pickle.load("/Dataset/DBv5/phychem/phychem-test-" + str(i + 1) + ".pkl")
+        model=CNNModel.CNN1D(shape=(WINSIZE*2+1,10))
+        model.fit(phychem_train_data,phychem_train_label,batch_size=100,epochs=100,
                       verbose=2)
-            y_pre=model.predict(x_test)
-            p_classes = [int(prob > 0.5) for prob in y_pre.flatten()]
-            eval.evalModel(size,y_pre,p_classes, y_test)
-        eval.evalTocsv()
+        y_pre=model.predict(phychem_test_data)
+        y_class = [int(prob > 0.5) for prob in y_pre.flatten()]
+        tp, fp, tn, fn, accuracy, precision, sensitivity, specificity, MCC, f1_score = \
+            tools.calculate_performace(len(y_class), y_class, phychem_test_label)
+        print(tp, fp, tn, fn, accuracy, precision, sensitivity, specificity, MCC, f1_score)
--- Pipeline/DBv5/Pssm.py
+++ Pipeline/DBv5/Pssm.py
@@ -1,28 +1,19 @@
 import csv
-# os.chdir('root directory')
-# sys.path.append('root directory')
-from keras.models import load_model
-from sklearn.model_selection import StratifiedKFold
-from Datas.Datasets import Datasets
+# os.chdir('your directory')
+# sys.path.append('your directory')
+import pickle
+from Pipeline import tools
 from Model.CNN.CnnModel import CNNModel
-from Result.Train.EvalModel import EvalModel
 def TrainPSSM():
-    winsizelist=[8]
-    for size in winsizelist:
-        data, label = Datasets.getPSSM('DBv5', size=size)
-        skf=StratifiedKFold(n_splits=10,random_state=0,shuffle=True)
-        eval=EvalModel()
-        modelIdx=0
-        for train,test in skf.split(data,label):
-            x_train=data[train];x_test=data[test]
-            y_train=label[train];y_test=label[test]
-            modelIdx += 1
-            model=CNNModel.CNN1D(shape=(size*2+1,20))
-            model.fit(x_train,y_train,batch_size=100,epochs=100,
-                      verbose=2)
-            #model.save(''+str(size)+'_'+ str(modelIdx)+'.h5')
-            #model = load_model(''+ str(modelIdx)+'.h5')
-            y_pre=model.predict(x_test)
-            p_classes = [int(prob > 0.5) for prob in y_pre.flatten()]
-            eval.evalModel(size,y_pre,p_classes, y_test)
-        eval.evalTocsv()
+    WINSIZE=8
+    for i in range(10):
+        pssm_train_data, pssm_train_label = pickle.load("/Dataset/DBv5/pssm/pssm-train-"+str(i + 1)+".pkl")
+        pssm_test_data, pssm_test_label = pickle.load("/Dataset/DBv5/pssm/pssm-test-" + str(i + 1) + ".pkl")
+        model=CNNModel.CNN1D(shape=(WINSIZE*2+1,20))
+        model.fit(pssm_train_data,pssm_train_label,batch_size=100,epochs=100,
+                  verbose=2)
+        y_pre=model.predict(pssm_test_data)
+        y_class = [int(prob > 0.5) for prob in y_pre.flatten()]
+        tp, fp, tn, fn, accuracy, precision, sensitivity, specificity, MCC, f1_score=\
+            tools.calculate_performace(len(y_class),y_class,pssm_test_label)
+        print(tp, fp, tn, fn, accuracy, precision, sensitivity, specificity, MCC, f1_score)
